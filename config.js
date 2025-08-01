f9bc70ea3c33c06b68dcb237f5bdaa9e57d87151
윤여찬 <ducks1077@ulalalab.com>
2025-08-01 14:31:53 +0900

    [add] 웨딩
    웨딩

---
+++ config.js
@@ -0,0 +1,21 @@
+export const WEDDING_INVITATION_URL = "http://localhost:8000/";
+export const KAKAOTALK_API_TOKEN = "a73479c588004db1adbb918270ba43f8";
+export const KAKAOTALK_SHARE_IMAGE =
+  "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
+
+export const WEDDING_DATE = "2025년 11월 01일, 토요일 오전 11시 00분";
+export const WEDDING_LOCATION = "안산 한양대 에리카웨딩홀, 1층";
+
+export const GROOM_NAME = "윤여찬";
+export const GROOM_ACCOUNT_NUMBER = "하나은행 620-2505-44468";
+export const GROOM_FATHER_NAME = "윤신석";
+export const GROOM_FATHER_ACCOUNT_NUMBER = "새마을금고 9003-2070-6100-9";
+export const GROOM_MOTHER_NAME = "임영숙";
+export const GROOM_MOTHER_ACCOUNT_NUMBER = "국민은행 35611-22-313193";
+
+export const BRIDE_NAME = "배유경";
+export const BRIDE_ACCOUNT_NUMBER = "국민은행 264401-04-398696";
+export const BRIDE_FATHER_NAME = "배경식";
+export const BRIDE_FATHER_ACCOUNT_NUMBER = "국민은행 592201-01-448729";
+export const BRIDE_MOTHER_NAME = "박기영";
+export const BRIDE_MOTHER_ACCOUNT_NUMBER = "농협은행 356-009-2963403";
---
+++ src/components/location.jsx
@@ -0,0 +1,142 @@
+import React, { useEffect } from "react";
+import { Divider } from "antd";
+import styled from "styled-components";
+import Flower from "../assets/flower2.png";
+
+const Wrapper = styled.div`
+  padding-top: 42px;
+  width: 70%;
+  margin: 0 auto;
+`;
+
+const Title = styled.span`
+  font-size: 1rem;
+  color: var(--title-color);
+  font-weight: bold;
+  opacity: 0.85;
+  margin-bottom: 0;
+`;
+
+const Image = styled.img`
+  display: block;
+  margin: 0 auto;
+  width: 1.375rem;
+  padding-bottom: 42px;
+`;
+
+const Content = styled.p`
+  font-size: 0.875rem;
+  line-height: 1.75;
+  opacity: 0.75;
+  width: 100%;
+  text-align: center;
+  padding-top: 42px;
+  padding-bottom: 42px;
+  margin: 0;
+`;
+
+const Map = styled.div`
+  width: 100%;
+  padding: 0;
+`;
+
+const Location = () => {
+  // 카카오 맵 불러오기
+
+  // 카카오 지도 불러오기
+  const executeScript = () => {
+    const scriptTag = document.createElement("script");
+    const inlineScript = document.createTextNode(`new daum.roughmap.Lander({
+      "timestamp" : "1754018521438",
+      "key" : "68yeyhr3875",
+      "mapWidth" : "640",
+      "mapHeight" : "360"
+    }).render();`);
+    scriptTag.appendChild(inlineScript);
+    document.body.appendChild(scriptTag);
+  };
+
+  // <!-- 2. 설치 스크립트 * 지도 퍼가기 서비스를 2개 이상 넣을 경우, 설치 스크립트는 하나만 삽입합니다. -->
+  // document.write 문제가 발생해서 해당 파일을 직접 가져온다음 수정했음
+  const InstallScript = () => {
+    (function () {
+      let c = window.location.protocol === "https:" ? "https:" : "http:";
+      let a = "16137cec";
+
+      if (window.daum && window.daum.roughmap && window.daum.roughmap.cdn) {
+        return;
+      }
+      window.daum = window.daum || {};
+      window.daum.roughmap = {
+        cdn: a,
+        URL_KEY_DATA_LOAD_PRE: c + "//t1.daumcdn.net/roughmap/",
+        url_protocal: c,
+      };
+      let b =
+        c +
+        "//t1.daumcdn.net/kakaomapweb/place/jscss/roughmap/" +
+        a +
+        "/roughmapLander.js";
+
+      // document.write -> doumnet.body.append로 수정
+      const scriptTag = document.createElement("script");
+      scriptTag.src = b;
+      document.body.append(scriptTag);
+      scriptTag.onload = () => {
+        executeScript();
+      };
+    })();
+  };
+
+  useEffect(() => {
+    InstallScript();
+  }, [InstallScript]);
+
+  return (
+    <Wrapper>
+      <Divider plain style={{ marginTop: 0, marginBottom: 32 }}>
+        <Title>오시는 길</Title>
+      </Divider>
+      <Image src={Flower} />
+      <Map
+        id="daumRoughmapContainer1754018521438"
+        className="root_daum_roughmap root_daum_roughmap_landing"
+      ></Map>
+      <Content>
+        경기 안산시 상록구 사동 1271
+        <br />
+        경기 안산시 상록구 한양대학로 55 에리카컨벤션센터
+        <br />
+        <br />
+        <Title>자차 이용시</Title>
+        <br />
+        <br />
+        주차 2,000대 가능
+        <br />
+        <br />
+        <Title>셔틀버스 이용시</Title>
+        <br />
+        <br />
+        "한대앞"역에서 하차 후 2번 출구로 나와
+        <br />
+        광장 횡단보도 앞 에리카컨벤션 셔틀버스 이용
+        <br />
+        <br />
+        <Title>버스 이용시</Title>
+        <br />
+        <br />
+        10-1, 3102 한양대 에리카컨벤션 하차
+        <br />
+        3100, 3101 한양대 하차
+        <br />
+        <br />
+        <Title>지하철 이용시</Title>
+        <br />
+        <br />
+        한대앞역 하차 (셔틀버스 이용)
+      </Content>
+    </Wrapper>
+  );
+};
+
+export default Location;
